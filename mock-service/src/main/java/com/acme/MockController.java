ad5d9abd0f818f8d16c2de25fda3ca2d30b09cfe
Meeraj Kunnumpurath <mkunnumpurath@gmail.com>
2019-01-21 07:25:36 +0400

    First commit

---
+++ mock-service/src/main/java/com/acme/MockController.java
@@ -0,0 +1,15 @@
+package com.acme;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MockController {
+
+    @RequestMapping("/mock")
+    public String greeting() {
+        return "mock";
+    }
+
+}
